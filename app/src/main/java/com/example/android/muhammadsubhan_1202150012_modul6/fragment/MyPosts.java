3796680969d93ea58032a531da55c58ad36c9590
SubhanAzmii <36130975+SubhanAzmii@users.noreply.github.com>
2018-04-01 20:23:55 +0700

    Update MyPosts.java

--- app/src/main/java/com/example/android/muhammadsubhan_1202150012_modul6/fragment/MyPosts.java
+++ app/src/main/java/com/example/android/muhammadsubhan_1202150012_modul6/fragment/MyPosts.java
@@ -3,9 +3,7 @@ package com.example.android.muhammadsubhan_1202150012_modul6.fragment;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.Query;
 
-/**
- * Created by Kizuna on 3/31/2018.
- */
+
 
 public class MyPosts extends PostList {
 
